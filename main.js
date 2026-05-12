d3b813a5ee13a5bea07bdd6326fc3a1e1d107e94
IngridDiazRisso <ingridiazrisso@gmail.com>
2026-05-12 22:09:06 +0200

    contador carrito

--- main.js
+++ main.js
@@ -27,3 +27,27 @@ prev.addEventListener("click", () => {
   if (indice < 0) indice = fotos.length - 1;
   mostrarFoto();
 });
+
+let cantidad = 0;
+let total = 0;
+
+const botones = document.querySelectorAll(".agregar");
+const cantidadHTML = document.getElementById("cantidad");
+const totalHTML = document.getElementById("total");
+const comprar = document.getElementById("comprar");
+
+botones.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    const precio = Number(boton.dataset.precio);
+
+    cantidad++;
+    total += precio;
+
+    cantidadHTML.textContent = cantidad;
+    totalHTML.textContent = "Total: " + total + "€";
+  });
+});
+
+comprar.addEventListener("click", () => {
+  alert("Compra ficticia realizada. No se ha hecho ningún pago real.");
+});
